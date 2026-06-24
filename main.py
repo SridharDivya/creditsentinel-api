704c0f7d27b854e8814f8e6205444363b7ca79a3
SridharDivya <sridhar.divya112005@gmail.com>
2026-06-24 10:13:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -836,4 +836,4 @@ def portfolio_summary():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
